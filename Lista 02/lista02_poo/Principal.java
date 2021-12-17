9e22a8786c0451c18d071f338477e5b394669b7b
Vinícius <vinicius.o.mendes@ufv.br>
2021-12-16 22:32:10 -0300

    up

--- Lista 02/lista02_poo/Principal.java
+++ Lista 02/lista02_poo/Principal.java
@@ -17,19 +17,19 @@ public class Principal {
         //Funcionário efetivado
         Funcionario funcionario1 = new Funcionario("Vinícius Mendes", "03/05/2001");;
         funcionario1.contratar(true, 1500, 0,"12/05/2020");
-        funcionario1.salarioBruto(170);
+        funcionario1.salarioBruto((float) 170);
         funcionario1.folhaPagamento();
         
         //Funcionário horista
         Funcionario funcionario2 = new Funcionario("Tainá Vieira", "01/03/2004");
         funcionario2.contratar(false, 15, 1,"02/01/2020");
-        funcionario2.salarioBruto(130);
+        funcionario2.salarioBruto((float) 130.78);
         funcionario2.folhaPagamento();
         
         //Funcionário efetivado
         Funcionario funcionario3 = new Funcionario("Adairson Geraldo", "31/08/1974");
         funcionario3.contratar(true, 10000, 3,"03/02/1998");
-        funcionario3.salarioBruto(160);
+        funcionario3.salarioBruto((float) 160);
         funcionario3.folhaPagamento();
         
         //Funcionário horista
@@ -38,11 +38,21 @@ public class Principal {
         funcionario4.salarioBruto((float) 120.5);
         funcionario4.folhaPagamento();
         
-        //Funcionário efetivado
+        //Funcionário horista
         Funcionario funcionario5 = new Funcionario("Milena de Oliveira", "10/11/2003");
         funcionario5.contratar(false, 9, 0,"29/07/2021");
         funcionario5.salarioBruto((float) 117.32);
         funcionario5.folhaPagamento();
+        
+        //testando get e set
+        Funcionario funcionario6 = new Funcionario("Maria Aparecida", "14/08/1973");
+        funcionario6.contratar(true, 1500, 0,"29/07/2006");
+        funcionario6.setTipoContrato(false);
+        funcionario6.setSalario(8);
+        System.out.println("Após a alteração, o novo tipo de contrato é ([true] efetivado [false] horista): " 
+                + funcionario6.isTipoContrato());
+        System.out.println("E o novo salário é: " + funcionario6.getSalario()+ "por hora") ;
+        
     }
 
 }
